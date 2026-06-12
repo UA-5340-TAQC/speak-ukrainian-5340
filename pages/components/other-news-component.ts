4bfb663a54c7c2c7cc0491dca867889699f5b26f
Lesia <lesia.liashko.shi.2024@lpnu.ua>
2026-06-12 10:40:54 +0300

    fix

--- pages/components/other-news-component.ts
+++ pages/components/other-news-component.ts
@@ -3,48 +3,46 @@ import { Locator, Page } from '@playwright/test';
 import { NewsCardComponent } from './news-card-component.js';
 
 export class OtherNewsComponent extends BaseComponent {
-    
-    private readonly carousel: Locator;
-    private readonly cards: Locator;
-    private readonly nextButton: Locator;
-    private readonly prevButton: Locator;
-    private readonly pagination: Locator;
-    private readonly newsCardComponent: NewsCardComponent;
+  private readonly carousel: Locator;
+  private readonly cards: Locator;
+  private readonly nextButton: Locator;
+  private readonly prevButton: Locator;
+  private readonly pagination: Locator;
+  private readonly newsCardComponent: NewsCardComponent;
 
-    constructor(page: Page, rootSelector: string) {
-        super(page, rootSelector);
-        this.carousel = this.root.locator('.other-news');
-        this.cards = this.carousel.locator('.carousel-item');
-        this.nextButton = this.carousel.locator('[aria-label="arrow-left"]');
-        this.prevButton = this.carousel.locator('[aria-label="arrow-right"]');
-        this.pagination = this.carousel.locator('ul.slick-dots');
-        this.newsCardComponent = new NewsCardComponent(page, rootSelector);
+  constructor(page: Page, rootSelector: string) {
+    super(page, rootSelector);
+    this.carousel = this.root.locator('.other-news');
+    this.cards = this.carousel.locator('.carousel-item');
+    this.nextButton = this.carousel.locator('[aria-label="arrow-left"]');
+    this.prevButton = this.carousel.locator('[aria-label="arrow-right"]');
+    this.pagination = this.carousel.locator('ul.slick-dots');
+    this.newsCardComponent = new NewsCardComponent(page, rootSelector);
+  }
+
+  async clickLeftArrow(): Promise<void> {
+    await this.nextButton.click();
+  }
+
+  async clickRightArrow(): Promise<void> {
+    await this.prevButton.click();
+  }
+
+  async getVisibleTitles(): Promise<string[]> {
+    const count = await this.cards.count();
+    const titles: string[] = [];
+
+    for (let i = 0; i < count; i++) {
+      const card = new NewsCardComponent(this.page, this.cards.nth(i).toString());
+      const title = await card.getNewsTitle();
+
+      titles.push(title ?? '');
     }
 
-    async clickLeftArrow(): Promise<void> {
-        await this.nextButton.click();
-    }
+    return titles;
+  }
 
-    async clickRightArrow(): Promise<void> {
-        await this.prevButton.click();
-    }
-
-    async getVisibleTitles(): Promise<string[]> {
-        const count = await this.cards.count();
-        const titles: string[] = [];
-
-        for (let i = 0; i < count; i++) {
-            const card = new NewsCardComponent(this.page, this.cards.nth(i).toString());
-            const title = await card.getNewsTitle();
-
-            titles.push(title ?? '');
-        }
-
-        return titles;
-    }
-
-    getCard(index: number): NewsCardComponent {
-        return new NewsCardComponent(this.page, this.cards.nth(index).toString());
-    }
-
+  getCard(index: number): NewsCardComponent {
+    return new NewsCardComponent(this.page, this.cards.nth(index).toString());
+  }
 }
