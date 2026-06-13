fb2fcd0826c93de2e6c6b015044a1eecaef41860
Lesia <lesia.liashko.shi.2024@lpnu.ua>
2026-06-13 21:11:08 +0300

    fixed all files

--- pages/components/other-news-component.ts
+++ pages/components/other-news-component.ts
@@ -1,50 +1,35 @@
-import { BaseComponent } from './base-component.js';
-import { Locator, Page } from '@playwright/test';
-import { NewsCardComponent } from './news-card-component.js';
+import type { Locator } from '@playwright/test';
+
+import { BaseComponent } from './base-component';
+import { NewsCardComponent } from './news-card-component';
 
 export class OtherNewsComponent extends BaseComponent {
-  private readonly carousel: Locator;
   private readonly cards: Locator;
   private readonly nextButton: Locator;
   private readonly prevButton: Locator;
   private readonly pagination: Locator;
-  private readonly newsCardComponent: NewsCardComponent;
 
-  constructor(page: Page, rootSelector: string) {
-    super(page, rootSelector);
-    this.carousel = this.root.locator('.other-news');
-    this.cards = this.carousel.locator('.carousel-item');
-    this.nextButton = this.carousel.locator('[aria-label="arrow-left"]');
-    this.prevButton = this.carousel.locator('[aria-label="arrow-right"]');
-    this.pagination = this.carousel.locator('ul.slick-dots');
-    this.newsCardComponent = new NewsCardComponent(page, rootSelector);
-  }
-
-  async clickLeftArrow(): Promise<void> {
-    await this.nextButton.click();
-  }
-
-  async clickRightArrow(): Promise<void> {
-    await this.prevButton.click();
-  }
-
-  async getVisibleTitles(): Promise<string[]> {
-    const count = await this.cards.count();
-    const titles: string[] = [];
-
-    for (let i = 0; i < count; i++) {
-      const item = this.cards.nth(i);
-      if (!(await item.isVisible())) continue;
-      const card = this.getCard(i);
-      const title = await card.getNewsTitle();
-
-      titles.push(title ?? '');
-    }
-
-    return titles;
+  constructor(rootLocator: Locator) {
+    super(rootLocator);
+    this.cards = this.root.locator('.carousel-item');
+    this.prevButton = this.root.locator('[aria-label="arrow-left"]');
+    this.nextButton = this.root.locator('[aria-label="arrow-right"]');
+    this.pagination = this.root.locator('ul.slick-dots');
   }
 
   getCard(index: number): NewsCardComponent {
-    return new NewsCardComponent(this.page, this.cards.nth(index).toString());
+    return new NewsCardComponent(this.cards.nth(index));
+  }
+
+  async clickLeftArrow(): Promise<void> {
+    await this.prevButton.click();
+  }
+
+  async clickRightArrow(): Promise<void> {
+    await this.nextButton.click();
+  }
+
+  async getPaginationCount(): Promise<number> {
+    return this.pagination.locator('li').count();
   }
 }
---
+++ pages/newsdetails-page.ts
@@ -0,0 +1,35 @@
+import { Locator, Page } from '@playwright/test';
+import { BasePage } from './base-page';
+
+import { OtherNewsComponent } from './components/other-news-component';
+
+export class NewsDetailsPage extends BasePage {
+  private readonly newsTitle: Locator;
+  private readonly newsDescription: Locator;
+  private readonly newsImage: Locator;
+  private readonly newsDate: Locator;
+  private readonly helpButton: Locator;
+  public readonly otherNews: OtherNewsComponent;
+  //initializing the social media component
+
+  constructor(page: Page) {
+    super(page);
+    this.newsTitle = page.locator('#major-title');
+    this.newsDescription = page.locator('#description');
+    this.newsImage = page.locator('.image');
+    this.newsDate = page.locator('#date');
+    this.helpButton = page.locator('button.donate-button');
+    this.otherNews = new OtherNewsComponent(page.locator('.other-news'));
+  }
+  async getNewsTitle(): Promise<string | null> {
+    return this.newsTitle.textContent();
+  }
+
+  async getNewsDescription(): Promise<string | null> {
+    return this.newsDescription.textContent();
+  }
+
+  async clickHelpButton(): Promise<void> {
+    await this.helpButton.click();
+  }
+}
